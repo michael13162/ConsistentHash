c5fa47310d288657015f2bdc2ba97f0bc8acf1f0
Simon Redman <simon@ergotech.com>
2018-11-29 14:14:38 -0700

    Implement SimulatorFile hashing

--- SimulatorFile.py
+++ SimulatorFile.py
@@ -18,17 +18,40 @@
 # Date:        28 November 2018
 # Description: Class representing a file which resides on one or more Servers and is requested by Clients
 
+import hashlib
+import random
+import string
+
+
+MAX_HASH_VALUE = 2**256 - 1  # SHA256 = 256 bits
+
+
+SALT1 = "JSM"
+SALT2 = "WRZ"
+
+
+def generate_random_file(size: int, key_length: int = 20) -> 'SimulatorFile':
+    key = str.join('', random.choices(string.ascii_letters + string.digits, k=key_length))
+    return SimulatorFile(key, size)
+
 
 class SimulatorFile:
-    SALT1 = "JSM"
-    SALT2 = "WRZ"
 
     def __init__(self, key: str, size: int):
         self.key = key
         self.size = size
 
     def hash1(self) -> int:
-        pass
+        input = self.key + SALT1
+        return self._hash(input)
 
     def hash2(self) -> int:
-        pass
+        input = self.key + SALT2
+        return self._hash(input)
+
+    def _hash(self, input: str) -> int:
+        hash = hashlib.sha256(input.encode())
+        return int(hash.hexdigest(), 16)
+
+    def __repr__(self):
+        return self.key
